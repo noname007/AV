82c58da66a1f1ab93e6a3f67f67456176de28a58
y.zz <yz0437@gmail.com>
2016-01-13 18:11:50 +0800

    config vs

--- simple-player/simple-player/simple-player.cpp
+++ simple-player/simple-player/simple-player.cpp
@@ -4,8 +4,26 @@
 #include "stdafx.h"
 
 
-int _tmain(int argc, _TCHAR* argv[])
+
+
+#include <stdio.h>
+
+#define __STDC_CONSTANT_MACROS
+
+extern "C"
 {
+	#include "libavcodec/avcodec.h"
+	#include "SDL2/SDL.h"
+};
+
+int main(int argc, char* argv[])
+{
+	printf("%s", avcodec_configuration());
+	if(SDL_Init(SDL_INIT_VIDEO)) {  
+		printf( "Could not initialize SDL - %s\n", SDL_GetError()); 
+	} else{
+		printf("Success init SDL");
+	}
 	return 0;
 }
 
