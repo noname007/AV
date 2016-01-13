2e6dd31ae86609585b1b28a8d84981106f784fca
y.zz <yz0437@gmail.com>
2016-01-13 17:18:50 +0800

    SDL显示练习，黑白显示没有成功

--- sdl-1/sdl-1/sdl-1.cpp
+++ sdl-1/sdl-1/sdl-1.cpp
@@ -15,6 +15,8 @@ extern "C"{
 
 int thread_exit = 0;
 
+int v_pause = 0x00;
+
 //int i = 0;
 int refresh_video(void * opaque)
 {
@@ -22,11 +24,21 @@ int refresh_video(void * opaque)
 	
 	while(thread_exit == 0)
 	{
+		if(v_pause)
+		{
+
+			SDL_Delay(1000);
+			continue;
+		}
+
 		SDL_Event event;
 		event.type = REFRESH_EVENT;
 		//printf("%d=================================>\n",i++);
 		SDL_PushEvent(&event);
-		SDL_Delay(40);
+		//x2 2倍速
+		//SDL_Delay(40);
+		SDL_Delay(20);
+
 	}
 	SDL_Event event;
 	thread_exit = 0;
@@ -65,9 +77,10 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	//yuv 数据一帧的宽高
 	const int pix_w = 640 ,pix_h = 360;
+	//const int pix_w = 320 ,pix_h = 180;
 	
 	// 显示窗口的宽高
-	int window_w = 640,window_h = 360;
+	int window_w = 700,window_h = 700;
 
 	const int bpp =12;
 	
@@ -77,7 +90,11 @@ int _tmain(int argc, _TCHAR* argv[])
 	//unsigned char buffer [pix_w *pix_h /8];
 	//buffer 大小分配不足，导致程序一直崩溃，差一点都是问题
 	unsigned char buffer [one_frame_yuv_data];
-
+	
+	//const int  black_white = one_frame_yuv_data*2/3 + 100; //内存稍微的分配的大一点，就会避免一些自己没有想到的内存溢出问题
+	//unsigned char black_withebuffer [black_white];
+	//
+	
 	/**
 	*SDL_Init初始化
 	*/
@@ -89,10 +106,8 @@ int _tmain(int argc, _TCHAR* argv[])
 		printf("SUCCESS");
 	}
 
-
 	//SDL_Window *window = SDL_CreateWindow("yangzz",,100,100,window_w,window_h,SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE);
 	SDL_Window *window = SDL_CreateWindow("yangzz",SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,window_w,window_h,SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE);
-	
 
 
 	if(!window)
@@ -100,13 +115,10 @@ int _tmain(int argc, _TCHAR* argv[])
 		//fail process
 	}
 
-	SDL_Renderer *render = SDL_CreateRenderer(window,-1,0);
-
-	Uint32 pixformat = SDL_PIXELFORMAT_IYUV;
-	SDL_Texture *txture = SDL_CreateTexture(render,pixformat,SDL_TEXTUREACCESS_STREAMING,pix_w,pix_h);
 
 	FILE *fpyuv = NULL;
 	char filename[] = "sintel_640_360.yuv";
+	//char filename[] = "test_yuv420p_320x180.yuv";
  	fpyuv = fopen(filename,"rb+");
 
 	if(fpyuv == NULL)
@@ -115,16 +127,26 @@ int _tmain(int argc, _TCHAR* argv[])
 		return -1;
 	}
 
+
+
+	SDL_Renderer *render = SDL_CreateRenderer(window,-1,0);
+
+	Uint32 pixformat = SDL_PIXELFORMAT_IYUV;
+	SDL_Texture *txture = SDL_CreateTexture(render,pixformat,SDL_TEXTUREACCESS_STREAMING,pix_w,pix_h);
+
 	SDL_Rect rect;
 
 	SDL_Event event;
 	SDL_Thread * thread = SDL_CreateThread(refresh_video,NULL,NULL);
 
+
+
 	while (1)
 	{
 		SDL_WaitEvent(&event);
 		//printf("%d************************\n",i++);
-		//printf("Event %x\n",event.type);
+		if(event.type != REFRESH_EVENT)
+			printf("Event %x\n",event.type);
 		//fflush(stdout);
 		if(event.type == REFRESH_EVENT)
 		{
@@ -135,15 +157,26 @@ int _tmain(int argc, _TCHAR* argv[])
 				fread(buffer,1,one_frame_yuv_data,fpyuv);
 			}
 
+
+			//memccpy(black_withebuffer,buffer,)
+			//memcpy(black_withebuffer,buffer,black_white);
+			//black_withebuffer[black_white] = '\0';
+			
+			
 			/**
 			*更新纹理
 			* pix_w ？
 			*/
 			SDL_UpdateTexture(txture,NULL,buffer,pix_w);
-			rect.x =0;
-			rect.y =0;
-			rect.w = window_w;
-			rect.h = window_h;
+			//SDL_UpdateTexture(txture,nullptr,black_withebuffer,pix_w);
+
+			/************************************************************************/
+			/*修改显示位置,视频显示大小                                   			*/ 
+			/************************************************************************/
+			rect.x =10;
+			rect.y =200;
+			rect.w = window_w/2;
+			rect.h = window_h/2;
 
 			SDL_RenderClear(render);
 			/**
@@ -158,6 +191,8 @@ int _tmain(int argc, _TCHAR* argv[])
 		{
 			SDL_GetWindowSize(window,&window_w,&window_h);
 			printf("window width*height:%d*%d\n",window_w,window_h);
+			printf("timestamp %d",event.common.timestamp);
+			
 			//fflush(stdout);
 		}
 		else if(event.type == SDL_QUIT)
@@ -169,6 +204,11 @@ int _tmain(int argc, _TCHAR* argv[])
 		{
 			break;
 		}
+		else if(event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_SPACE)
+		{
+			v_pause ^= 0x01;
+		}
+	
 		
 	}
 
